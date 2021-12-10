431a19550c5f9ea3e021b6450f4b2fc998df4446
Prabha <nikprabha90@gmail.com>
2021-12-10 19:09:59 +0530

    Initial Commit

---
+++ StreamingMovie/StreamingMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamingMovieApp.swift
+//  StreamingMovie
+//
+//  Created by Prabagaran, Ganesan (G.) on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StreamingMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
